85a7963ada635424df120248261ab7e2f7155636
architectdevops7 <architectdevops7@gmail.com>
2023-05-24 16:28:54 +0530

    First commit to terraform files

---
+++ terra-c1/main.tf
@@ -0,0 +1,43 @@
+# Provider configuration
+provider "aws" {
+  region = "us-west-2"  # Replace with your desired AWS region
+}
+
+# Security Group resources
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Security group for web servers"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance resource
+resource "aws_instance" "web_instance" {
+  ami           = "ami-0c94855ba95c71c99"  # Replace with your desired AMI ID
+  instance_type = "t2.micro"
+
+  security_group = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "web-instance"
+  }
+}
